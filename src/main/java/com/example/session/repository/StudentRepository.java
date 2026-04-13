6a36bd08e91b0090226d671e6c1191d8e38eebfe
HYERI LEE <zzangga740890@gmail.com>
2026-04-13 21:43:06 +0900

    feat : 세션 실습

---
+++ src/main/java/com/example/session/repository/StudentRepository.java
@@ -0,0 +1,10 @@
+package com.example.session.repository;
+
+import com.example.session.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+    Optional<Student> findByStudentNumber(String studentNumber);
+}
