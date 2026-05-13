841b6390c25fe893a75fce3e1562d12e3b631b45
garden <113418319+Garden0728@users.noreply.github.com>
2026-05-13 10:54:33 +0900

    refactor: requestDto 수정

--- session/src/main/java/com/likelion14/session/Dto/group/GroupRequestDto.java
+++ session/src/main/java/com/likelion14/session/Dto/group/GroupRequestDto.java
@@ -2,6 +2,8 @@ package com.likelion14.session.Dto.group;
 
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
@@ -11,6 +13,7 @@ import lombok.NonNull;
 public class GroupRequestDto {
     @NotBlank(message = "그룹 이름은 필수입니다.")
     private String name;
-    @NotBlank(message = "인원 제한은 필수입니다.")
+    @NotNull(message = "인원 제한은 필수입니다.")
+    @Positive(message = "인원 제한 수는 양수여야 합니다.")
     private Integer maxUserCount;
 }
--- session/src/main/java/com/likelion14/session/Dto/user/UserCreateRequestDto.java
+++ session/src/main/java/com/likelion14/session/Dto/user/UserCreateRequestDto.java
@@ -1,6 +1,8 @@
 package com.likelion14.session.Dto.user;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
@@ -14,7 +16,8 @@ public class UserCreateRequestDto {
     private String userPw;  //비밀번호
     @NotBlank(message = "이름은 필수입니다.")
     private String userName; //이름
-    @NotBlank(message = "나이는 필수입니다.")
+    @NotNull(message = "나이는 필수입니다.")
+    @Positive(message = "나이는 양수여야 합니다.")
     private Integer age; //나이
     @NotBlank(message = "전화번호는 필수입니다.")
     private String phoneNum; //전화번호
